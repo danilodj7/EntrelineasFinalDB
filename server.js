0c4fc3f6b24cdece61489e7bb0141e72a4d1d40f
Danilo Andres <daniloandresj@gmail.com>
2021-10-17 19:35:58 -0500

    se creo el app.get con res.send

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ import  Express  from "express";
 // app se agrega todo eje rutas 
 const app = Express();
 
+//el mensaje lo mustra el servidor en la terminal de visual studio code
+app.get('/usuarios',(req,res)=>{
+    console.log('alguien hizo get en la ruta /usuarios')
+    res.send('usuarios no hay')
+});
+
 //escucha solicitudes con .list, se pone el puerto 5000 porque react tiene el puerto 3000
 app.listen(5000,() => {
     console.log('Escuchando el pueto 5000')
