644c7a74a6800df3395ef93e0938452c45cd02c8
Danilo Andres <daniloandresj@gmail.com>
2021-10-24 01:57:41 -0500

    Se importo jwt y jwks para la autenticacion

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ import Cors from 'cors';
 import dotenv from 'dotenv'
 import { conectarBd,getDB } from "./db/db.js";
 //importar mongodb
+
 import rutasUsuarios from "./views/usuarios/rutas.js";
 import rutasVentas from "./views/ventas/rutas.js";
 import rutasProductos from "./views/productos/rutas.js";
 
+import jwt from 'express-jwt'
+import jwks from 'jwks-rsa'
 
 
 dotenv.config({path:'./.env'})
@@ -18,6 +21,21 @@ dotenv.config({path:'./.env'})
 const app = Express();
 app.use(Express.json());
 app.use(Cors());
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://ventas-productoscar.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'api-autenticacion-productos-carros',
+issuer: 'https://ventas-productoscar.us.auth0.com/',
+algorithms: ['RS256']
+});
+
+app.use(jwtCheck )
+
 app.use(rutasUsuarios)
 app.use(rutasVentas)
 app.use(rutasProductos)
